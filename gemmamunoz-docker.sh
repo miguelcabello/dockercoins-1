658cee95503f228d55083ec74d3b09ceacc5dc68
gemmamunoz <68118917+gemmamunoz@users.noreply.github.com>
2022-03-15 12:55:31 +0100

    Update gemmamunoz-docker.sh

--- gemmamunoz-docker.sh
+++ gemmamunoz-docker.sh
@@ -2,17 +2,19 @@ git clone https://github.com/gemmamunoz/dockercoins-1
 cd dockercoins
 docker network create dockercoins
 docker login
-docker build -t gemmamunoz/ruby:sinatra-thin hasher/
-docker push gemmamunoz/ruby:sinatra-thin
-docker run -d --entrypoint ruby --name hasher --read-only --restart always -u nobody -v $PWD/hasher/hasher.rb:/data/hasher.rb -w /data/ --network dockercoins gemmamunoz/ruby:sinatra-thin hasher.rb
+docker build -t gemmadockerid/ruby:sinatra-thin hasher/
+docker push gemmadockerid/ruby:sinatra-thin
+docker run -d --entrypoint ruby --name hasher --read-only --restart always -u nobody -v $PWD/hasher/hasher.rb:/data/hasher.rb -w /data/ --network dockercoins gemmadockerid/ruby:sinatra-thin hasher.rb
 docker pull redis:alpine
 docker inspect redis:alpine
-docker build -t gemmamunoz/redis:alpine redis/ 
-docker push gemmamunoz/redis:alpine
-docker run -d --entrypoint docker-entrypoint.sh --name redis --read-only --restart always -u redis -v redis:/data/ -w /data/ --network dockercoins gemmamunoz/redis:alpine redis-server
-docker build -t gemmamunoz/python:flask rng/
-docker push gemmamunoz/python:flask
-docker run -d --entrypoint python --name rng --read-only --restart always -u nobody -v $PWD/rng/rng.py:/data/rng.py -w /data/ --network dockercoins gemmamunoz/python:flask rng.py
-docker build -t gemmamunoz/python:redis-requests worker/
-docker push gemmamunoz/python:redis-requests
-docker run -d --entrypoint python --name worker --read-only --restart always -u nobody -v $PWD/worker/worker.py:/data/worker.py -w /data/ --network dockercoins gemmamunoz/python:redis-requests worker.py 
+docker build -t gemmadockerid/redis:alpine redis/ 
+docker push gemmadockerid/redis:alpine
+docker run -d --entrypoint docker-entrypoint.sh --name redis --read-only --restart always -u redis -v redis:/data/ -w /data/ --network dockercoins gemmadockerid/redis:alpine redis-server
+docker build -t gemmadockerid/python:flask rng/
+docker push gemmadockerid/python:flask
+docker run -d --entrypoint python --name rng --read-only --restart always -u nobody -v $PWD/rng/rng.py:/data/rng.py -w /data/ --network dockercoins gemmadockerid/python:flask rng.py
+docker build -t gemmadockerid/python:redis-requests worker/
+docker push gemmadockerid/python:redis-requests
+docker run -d --entrypoint python --name worker --read-only --restart always -u nobody -v $PWD/worker/worker.py:/data/worker.py -w /data/ --network dockercoins gemmadockerid/python:redis-requests worker.py 
+docker build -t gemmadockerid/node:4-slim webui/
+docker push gemmadockerid/node:4-slim
